729b68f754208500a058a3fe2ebf6d8d3c6fc592
cyndicheng <71244584+cyndicheng@users.noreply.github.com>
2020-11-20 12:58:28 -0800

    Add files via upload

---
+++ project_pytest/test_chancecards_fail.py
@@ -0,0 +1,27 @@
+import pytest
+
+# When stop on space 7, 22 and 36 on the board, chance cards are triggered
+# Designate fixture to return list of 3 values (7, 22 and 36) on the board
+# If the space landed is not in the list, then cards are not triggered, so fail
+# If the space landed is in the list, then cards are triggered, so pass
+
+@pytest.fixture
+
+def cards_A_B_C(): 
+	a=7
+	b=22
+	c=36
+	return [a,b,c]
+
+# When z = 35, tests should fail because not 7, 22 or 36
+def test_comparewithA(cards_A_B_C):
+	z=35
+	assert cards_A_B_C[0]==z,"a and z comparison failed"
+
+def test_comparewithB(cards_A_B_C):
+	z=35
+	assert cards_A_B_C[1]==z,"b and z comparison failed"
+
+def test_comparewithC(cards_A_B_C):
+	z=35
+	assert cards_A_B_C[2]==z,"c and z comparison failed"
